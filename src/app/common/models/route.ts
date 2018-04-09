160757efa2cc986b4a714893ed653761d4ceefad
Justin Phillips <euangelion3o9@gmail.com>
2018-04-09 10:51:43 -0400

    Initial React Project

--- src/app/common/models/route.ts
+++
@@ -1,9 +0,0 @@
-import { Location } from 'app/common/models/location';
-
-export class Route {
-  origin: Location;
-  destination: Location;
-  distance: number;
-  duration: number;
-  cost: number;
-}
