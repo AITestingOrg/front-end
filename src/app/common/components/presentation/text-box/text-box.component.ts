95535d94fa6f24611cf7502703cbc465b8694ede
Richard Jimenez <Rjime061@fiu.edu>
2018-07-25 17:27:09 -0400

    feature/TOOL-7507-GPSLocator (#12)
    * implemented click on map feature where you will be able to get estimate based on clicking on map for pickup and destination
    
    * removed commented out code
    
    * Fix GPS locator-textbox interactions

--- src/app/common/components/presentation/text-box/text-box.component.ts
+++ src/app/common/components/presentation/text-box/text-box.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.scss']
 })
-export class TextBoxComponent implements OnInit {
+export class TextBoxComponent implements OnInit, OnChanges {
+
   @Input() private id: string;
   @Input() private placeholder: string;
   @Input() private encryption: string;
   @Input() private icon: string;
   @Input() private textboxValue: string;
-
-  @Output() textboxChange: EventEmitter<String> = new EventEmitter<String>();
+  ngOnChanges(changes: SimpleChanges): void {}
 
   constructor() { }
 
